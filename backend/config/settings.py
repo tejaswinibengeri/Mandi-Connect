38666f36251b6808456d2e7f133ffc4b4576797f
tejaswinibengeri <tejaswinibengeri02@gmail.com>
2026-03-12 22:48:26 +0530

    switch database to sqlite for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,14 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mandi_connect_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
